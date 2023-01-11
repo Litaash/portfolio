16d6d800b83a29a11db04c3c12d00aafb928b1f7
Oleksandr Litash <litash@MacBook-Air-Oleksandr.local>
2023-01-12 00:20:49 +0200

    setup project on next.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  reactStrictMode: false,
+  sassOptions: {
+    additionalData: `@import "_variables.scss";`,
+ }
 }
 
 module.exports = nextConfig
